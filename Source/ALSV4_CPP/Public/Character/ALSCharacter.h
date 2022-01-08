6563ff7b3932bb7fdddc8ac298216d3eeed18883
EPICGameGuy <admin@cactus.vg>
2022-01-08 17:18:10 -0500

    Removed some unnecessary crap

--- Source/ALSV4_CPP/Public/Character/ALSCharacter.h
+++ Source/ALSV4_CPP/Public/Character/ALSCharacter.h
@@ -24,46 +24,21 @@ public:
 	AALSCharacter(const FObjectInitializer& ObjectInitializer);
 
 	/** Implemented on BP to update held objects */
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ALS|HeldObject")
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ALS|HeldObject")
 	void UpdateHeldObject();
 
-	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
+	virtual void UpdateHeldObject_Implementation() {};
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "ALS|HeldObject")
 	void ClearHeldObject();
 
-	UFUNCTION(BlueprintCallable, Category = "ALS|HeldObject")
-	void AttachToHand(UStaticMesh* NewStaticMesh, USkeletalMesh* NewSkeletalMesh,
-	                  class UClass* NewAnimClass, bool bLeftHand, FVector Offset);
-
-	virtual void RagdollStart() override;
-
-	virtual void RagdollEnd() override;
+	virtual void ClearHeldObject_Implementation() {};
 
 	virtual ECollisionChannel GetThirdPersonTraceParams(FVector& TraceOrigin, float& TraceRadius) override;
 
 	virtual FTransform GetThirdPersonPivotTarget() override;
 
 	virtual FVector GetFirstPersonCameraTarget() override;
-
-protected:
-	virtual void Tick(float DeltaTime) override;
-
-	virtual void BeginPlay() override;
-
-	virtual void OnOverlayStateChanged(EALSOverlayState PreviousState) override;
-
-	/** Implement on BP to update animation states of held objects */
-	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ALS|HeldObject")
-	void UpdateHeldObjectAnimations();
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
-	USceneComponent* HeldObjectRoot = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
-	USkeletalMeshComponent* SkeletalMesh = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS|Component")
-	UStaticMeshComponent* StaticMesh = nullptr;
 
 private:
 	bool bNeedsColorReset = false;
